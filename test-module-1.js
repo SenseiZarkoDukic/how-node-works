26730a1b13bfb06ccae67f1bd27e2bf6f65fc5d2
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-03-13 18:23:22 +0100

    Refactor code to use ES6 module syntax and add new test module

--- test-module-1.js
+++ test-module-1.js
@@ -1,4 +1,22 @@
-class Calculator {
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+
+//   subtract(a, b) {
+//     return a - b;
+//   }
+
+//   multiply(a, b) {
+//     return a * b;
+//   }
+
+//   divide(a, b) {
+//     return a / b;
+//   }
+// }
+
+module.exports = class {
   add(a, b) {
     return a + b;
   }
@@ -14,6 +32,4 @@ class Calculator {
   divide(a, b) {
     return a / b;
   }
-}
-
-module.exports = Calculator;
+};
