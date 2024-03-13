dfadd656cf096d9d8b2c0c1bc654b3bb453129b5
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-03-13 18:29:33 +0100

    Add multiply function to test-module-2

--- modules.js
+++ modules.js
@@ -7,3 +7,7 @@ const calc1 = new C();
 console.log(calc1.add(2, 5));
 
 // exports
+// const calc2 = require("./test-module-2");
+// console.log(calc2.multiply(2, 5));
+const { add, subtract, multiply, divide } = require("./test-module-2");
+console.log(multiply(2, 5));
